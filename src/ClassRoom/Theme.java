186019eda9e5ec9b6fe3f0984f8e9b82568ce2d3
HS-JNYLee <2071013@hansung.ac.kr>
2024-12-13 01:48:59 +0900

    [ui]: Theme Color class

---
+++ src/ClassRoom/Theme.java
@@ -0,0 +1,16 @@
+package ClassRoom;
+
+import java.awt.*;
+
+public class Theme {
+    public static Color Green       = new Color(0x25AA8F);
+    public static Color LightGreen  = new Color(0x3AB174);
+    public static Color Black       = new Color(0x2A3847);
+    public static Color Blue        = new Color(0x1B74E7);
+    public static Color Orange      = new Color(0xFD743A);
+    public static Color White       = new Color(0xFFFFFF);
+    public static Color Yellow      = new Color(0xE3BE3C);
+    public static Color Red         = new Color(0xFA5B57);
+    public static Color Ultramarine = new Color(0x2C374C);
+    public static Color Grey        = new Color(0xD7DBDC);
+}
