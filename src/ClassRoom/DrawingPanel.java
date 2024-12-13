3db411e607f0e9dc84ce17bc8aaa3d44f1954f65
HS-JNYLee <2071013@hansung.ac.kr>
2024-12-13 21:33:59 +0900

    [refactor]: drawing panel

--- src/ClassRoom/DrawingPanel.java
+++ src/ClassRoom/DrawingPanel.java
@@ -10,14 +10,20 @@ import java.awt.image.BufferedImage;
 
 // Panel for drawing annotations
 public class DrawingPanel extends JPanel {
-    private BufferedImage drawingImage;
-    private int eraserSize = 20;
+    private final BufferedImage drawingImage;
     private Color color;
+    private PaletteButton penButton;
+    private PaletteButton eraserButton;
     public void setColor(Color color) {
         this.color = color;
     }
 
-    public DrawingPanel(PaletteButton penButton, PaletteButton eraserButton) {
+    public void setButtons(PaletteButton penButton, PaletteButton eraserButton) {
+        this.penButton = penButton;
+        this.eraserButton = eraserButton;
+    }
+
+    public DrawingPanel() {
         drawingImage = new BufferedImage(600, 340, BufferedImage.TYPE_INT_ARGB);
         MouseAdapter adapter = new MouseAdapter() {
             private Point prevPoint;
@@ -33,7 +39,7 @@ public class DrawingPanel extends JPanel {
             @Override
             public void mouseDragged(MouseEvent e) {
                 if (penButton.getActive()) {
-                    drawLine(e.getPoint(), color, 2, prevPoint); // 연필은 빨간색
+                    drawLine(e.getPoint(), color, prevPoint); // 연필은 빨간색
                 } else if (eraserButton.getActive()) {
                     erase(e.getPoint()); // 지우개 기능
                 }
@@ -47,15 +53,16 @@ public class DrawingPanel extends JPanel {
     private void erase(Point point) {
         Graphics2D g = drawingImage.createGraphics();
         g.setComposite(AlphaComposite.Clear); // 투명하게 지우기
+        int eraserSize = 20;
         g.fillRect(point.x - eraserSize / 2, point.y - eraserSize / 2, eraserSize, eraserSize);
         g.dispose();
         repaint();
     }
 
-    private void drawLine(Point currPoint, Color color, int strokeWidth, Point prevPoint) {
+    private void drawLine(Point currPoint, Color color, Point prevPoint) {
         Graphics2D g = drawingImage.createGraphics();
         g.setColor(color);
-        g.setStroke(new BasicStroke(strokeWidth));
+        g.setStroke(new BasicStroke(2));
         g.drawLine(prevPoint.x, prevPoint.y, currPoint.x, currPoint.y);
         g.dispose();
         repaint();
