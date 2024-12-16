a7b9c79b5a343856103709898585a022980c4e16
HS-JNYLee <2071013@hansung.ac.kr>
2024-12-17 00:21:38 +0900

    [refactor]: DatabaseFile refactor

--- src/ClassRoom/DatabaseFile.java
+++ src/ClassRoom/DatabaseFile.java
@@ -10,11 +10,7 @@ import java.util.*;
 public class DatabaseFile {
     static private ArrayList<User> userList;
 
-    public static ArrayList<User> getUserList() {
-        return userList;
-    }
-
-    static public ArrayList<User> parsing() {
+    static public void parsing() {
         // 텍스트 파일 경로
         String filePath = "src/account.txt";
 
@@ -26,7 +22,7 @@ public class DatabaseFile {
             String line;
 
             while ((line = br.readLine()) != null) {
-                // 각 라인을 ','로 분리
+                // , 로 분리
                 String[] parts = line.split(",");
 
                 // 데이터 검증
@@ -36,17 +32,16 @@ public class DatabaseFile {
                 }
 
                 // 데이터 추출 및 Person 객체 생성
-                String name = parts[0].trim();
-                String id = parts[1].trim();
-                String type = parts[2].trim();
-                String iconPath = parts[3].trim();
-                String teamRoomAddr = parts[4].trim();
-                String userTableIndex = parts[5].trim(); // 출석용도 지정 좌석 번호
+                String name             = parts[0].trim();
+                String id               = parts[1].trim();
+                String type             = parts[2].trim();
+                String iconPath         = parts[3].trim();
+                String teamRoomAddr     = parts[4].trim();
+                String userTableIndex   = parts[5].trim(); // 출석용도 지정 좌석 번호
 
                 User user = new User();
                 user.setName(name);
                 user.setId(id);
-                user.setUserTableIndex(Integer.parseInt(userTableIndex));
 
                 File file = new File(iconPath);
                 if (file.exists()) {
@@ -54,26 +49,15 @@ public class DatabaseFile {
                     user.setProfileImage(icon);
                 }
                 user.setTeamRoomAddr(Integer.parseInt(teamRoomAddr));
+                user.setUserTableIndex(Integer.parseInt(userTableIndex));
 
                 user.setRole(matchRole(type));
                 userList.add(user);
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage() + e.getCause());
         }
 
-        // 결과 디버깅
-        for (User user : userList) {
-            System.out.println(user);
-        }
-
-        return userList;
-    }
-
-    private User user;
-
-    public User getUser() {
-        return user;
     }
 
     public boolean isValidate(User user) {
@@ -85,7 +69,6 @@ public class DatabaseFile {
                 user.setUserTableIndex(user1.getUserTableIndex());
                 user.setTeamRoomAddr(user1.getTeamRoomAddr());
                 user.setName(user1.getName());
-                this.user = user1;
                 return true;
             }
         }
